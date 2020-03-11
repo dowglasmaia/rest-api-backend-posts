03c9ae1c7ec5bddfb0102c79b8d82d81cd6908a6
Dowglas Maia <dowglasmaia@gmail.com>
2020-03-10 23:51:23 -0300

    UsuarioDTO

--- src/main/java/com/maia/controller/UsuarioController.java
+++ src/main/java/com/maia/controller/UsuarioController.java
@@ -1,6 +1,7 @@
 package com.maia.controller;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.maia.domain.Usuario;
+import com.maia.domain.dto.UsuarioDTO;
 import com.maia.services.UsuarioService;
 
 @RestController
@@ -18,10 +20,11 @@ public class UsuarioController {
 	@Autowired
 	private UsuarioService serive;
 
-	@GetMapping	
-	public ResponseEntity<List<Usuario>> getAll() {
-		List<Usuario> result = serive.findAll();	
-		return ResponseEntity.ok().body(result);
+	@GetMapping
+	public ResponseEntity<List<UsuarioDTO>> getAll() {
+		List<Usuario> lista = serive.findAll();
+		List<UsuarioDTO> listaDTO = lista.stream().map(obj -> new UsuarioDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listaDTO);
 
 	}
 
---
+++ src/main/java/com/maia/domain/dto/UsuarioDTO.java
@@ -0,0 +1,51 @@
+package com.maia.domain.dto;
+
+import java.io.Serializable;
+
+import com.maia.domain.Usuario;
+
+public class UsuarioDTO implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	private String id;
+	private String nome;
+	private String email;
+
+	public UsuarioDTO() {
+		
+	}
+
+	public UsuarioDTO(Usuario usuario) {
+		super();
+		this.id = usuario.getId();
+		this.nome = usuario.getNome();
+		this.email = usuario.getEmail();
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+
+}
