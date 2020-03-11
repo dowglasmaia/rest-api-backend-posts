e3b95e31b157412de1d2b1369fa5185abfd822a2
Dowglas Maia <dowglasmaia@gmail.com>
2020-03-10 22:29:57 -0300

    Start Project

---
+++ src/test/java/com/maia/PostsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.maia;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PostsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
