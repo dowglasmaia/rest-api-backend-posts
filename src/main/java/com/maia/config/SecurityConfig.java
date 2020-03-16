546bd02c8acd9b6106bdc19e62af586a8b5c81b9
Dowglas Maia <dowglasmaia@gmail.com>
2020-03-16 06:46:59 -0300

    impl-> config Swagger

--- src/main/java/com/maia/config/SecurityConfig.java
+++ src/main/java/com/maia/config/SecurityConfig.java
@@ -8,8 +8,45 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@EnableSwagger2
 @Configuration
 public class SecurityConfig {
+	
+	@Bean
+	public Docket docket () {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.apis(RequestHandlerSelectors.basePackage("com.maia.controller"))
+				.paths(PathSelectors.any())
+				.build()
+				.apiInfo(apiInfo());		
+	}
+	
+	private ApiInfo apiInfo() {
+		return  new ApiInfoBuilder()
+				.title("API BACK-END MAIA POSTS")
+				.description("Sistema de Postagem Aberta ")
+				.version("1.0")
+				.contact(contact())
+				.build();		
+	}
+	
+	private Contact contact() {
+		return new Contact("Dowglas Maia", 
+				"https://github.com/dowglasmaia", 
+				"dowglasmaia@live.com");
+	}
+	
 	
 	@Bean
 	CorsConfigurationSource corsConfigurationSource() {
@@ -21,4 +58,5 @@ public class SecurityConfig {
 
 	}
 
+	
 }
